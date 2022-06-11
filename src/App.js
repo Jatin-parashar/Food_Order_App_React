9335b569de77b73bc22c4ee8636a0c59cd922cb3
Jatin Parashar <parashar18jatin@gmail.com>
2022-06-11 12:24:25 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
-    <Header />
-    <main>
-    <Meals />
-    </main>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import classes from "./MealItem.module.css";
+import MealsItemForm from "./MealItemForm";
+
+const MealItem = props =>{
+    const price =  `₹${props.price.toFixed(2)}`;
+    return <li className={classes.meal}>
+        <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+        <MealsItemForm id={props.id}/>
+        </div>
+    </li>
+}
+
+export default MealItem;
